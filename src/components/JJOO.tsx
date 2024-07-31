f13b8203f3a380d3b9618be0c155e594fb11f4a1
Adrian Moreno <luiadrimore219@gmail.com>
2024-07-31 08:37:58 -0600

    Prueba dos con reverse

---
+++ src/components/JJOO.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const JJOO = () => {
+  return (
+    <>
+        <div className='bg-success w-100'>
+            a
+        </div>
+    </>
+  )
+}
+
+export default JJOO
